68912d468570f14523e714a3b5032e0d3bd26b5f
zojusane <2016301200314@whu.edu.cn>
2021-01-14 20:36:54 +0800

    test pycharm commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi():
+    # Use a breakpoint in the code line below to debug your script.
+    a = ['a', 'b', 'c', 'd']
+    b = a[0:2]
+    b[0] = 'q'
+    print(b)
+    print(a)
+    print(a, b)
+    print(b, a)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
